67d45459579e9c17baca9b5732c134221093d013
josephmaxwellhyland <josephmaxwellhyland@users.noreply.github.com>
2025-10-24 17:07:09 +0000

    Update gitbook 2025-10-24 17:07:08

---
+++ Heist/puzzleScript.js
@@ -0,0 +1,24 @@
+function calculate() {
+	let x = 500;
+	let y = 2;
+	let z = 5;
+
+	let g = x / y + z;
+	let q = y + 10;
+	
+	if (q < y)
+  {
+		g = g - 5; y = y / y;
+		q = q + 9 - 1 * 7 + 4 / 3 - 1;
+	}
+  else
+  {
+		x = x / 100 + 2 * 2;
+		g = (x - y - z) * -1;
+		q = 2 * ((g + x) / y);
+	}
+	
+	console.log(q);
+}
+
+calculate();
